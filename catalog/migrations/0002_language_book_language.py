1c02326f9c0dc37342e31c3eb8734fbc9b71cfa9
Shaun203 <97681363+Shaun203@users.noreply.github.com>
2026-09-08 18:13:16 -0400

    Part 3

---
+++ catalog/migrations/0002_language_book_language.py
@@ -0,0 +1,24 @@
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('catalog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Language',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100, unique=True)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='language',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.RESTRICT, to='catalog.language'),
+        ),
+    ]
